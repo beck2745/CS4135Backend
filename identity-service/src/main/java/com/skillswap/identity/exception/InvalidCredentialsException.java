250a069c0cb8c4832abe6f23af90b318541e662c
22357564becky <beckymeade2745@gmail.com>
2026-04-13 23:21:54 +0100

    Adding auth DTOs and exception handling

---
+++ identity-service/src/main/java/com/skillswap/identity/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.skillswap.identity.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
