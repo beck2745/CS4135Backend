a5ef918cbcfd64b7d4393d49b76774191c34e1ab
ZoeDunworth123 <22338594@studentmail.ul.ie>
2026-04-13 20:22:56 +0100

    Small error fix

--- api-gateway/src/main/java/com/skillshare/gateway/APIGatewayApplication.java
+++ api-gateway/src/main/java/com/skillshare/gateway/APIGatewayApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class APIGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(APIGatewayApplication.class, args);
     }
 }
