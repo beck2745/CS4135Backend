509387ef93425989be42f4789e587b6a8628c4f6
ZoeDunworth123 <22338594@studentmail.ul.ie>
2026-04-13 20:25:43 +0100

    Package name fixes

--- api-gateway/src/main/java/com/skillswap/gateway/APIGatewayApplication.java
+++ api-gateway/src/main/java/com/skillswap/gateway/APIGatewayApplication.java
@@ -1,12 +1,12 @@
-package com.skillshare.identity;
+package com.skillswap.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IdentityServiceApplication {
+public class APIGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IdentityServiceApplication.class, args);
+        SpringApplication.run(APIGatewayApplication.class, args);
     }
 }
--- identity-service/src/main/java/com/skillshare/identity/web/ScaffoldHealthController.java
+++ identity-service/src/main/java/com/skillshare/identity/web/ScaffoldHealthController.java
@@ -1,4 +1,4 @@
-package com.skillswap.identity.web;
+package com.skillshare.identity.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
