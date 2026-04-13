3ffd2c85568f8a777ac4e2f3755a975e888db1f3
ZoeDunworth123 <22338594@studentmail.ul.ie>
2026-04-13 23:58:06 +0100

    REST and internal booking controllers

--- booking-service/src/main/java/com/skillswap/booking/BookingServiceApplication.java
+++ booking-service/src/main/java/com/skillswap/booking/BookingServiceApplication.java
@@ -2,7 +2,13 @@ package com.skillswap.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableScheduling
 @SpringBootApplication
 public class BookingServiceApplication {
 
