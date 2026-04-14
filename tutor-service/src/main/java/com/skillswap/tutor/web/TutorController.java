0d0ac0ea48ff56acdc5226473475eac5c7c3ddf0
maevebray <22345744@studentmail.ul.ie>
2026-04-14 12:07:39 +0100

    remove hardcoded CORS origins from controllers and enable native profile in configuration services

--- tutor-service/src/main/java/com/skillswap/tutor/web/TutorController.java
+++ tutor-service/src/main/java/com/skillswap/tutor/web/TutorController.java
@@ -18,7 +18,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tutors")
-@CrossOrigin(origins = "http://localhost:5173")
 public class TutorController {
 
     private final TutorProfileService tutorProfileService;
